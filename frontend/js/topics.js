a21c3737e0d65d136ad508875c02d821889c6168
unknown <xtofl_2@.(none)>
2013-11-30 15:46:47 +0100

    added jquery and require; moved topics enumeration into topics.js

---
+++ frontend/js/topics.js
@@ -0,0 +1,32 @@
+/**
+ * @author xtofl_2
+ */
+
+define( 
+	["jquery"], 
+	function($)
+	{
+		var createElementForTopic = function(topic) {
+			var element = document.createElement("li");
+			element.innerText = topic;
+			return element;
+		};
+		var elements = 
+		["stress-management",
+		"leerstrategie",
+		"zelfbeeld",
+		"socio-emotionele ontwikkeling",
+		"organisatie",
+		"adminstratie",
+		"begeleidingsstrategie"];
+		
+		return {
+			elements: function(){
+				return elements;
+			},
+			appendToElement: function(e) {
+				elements.forEach(function(element){
+					$(e).append(createElementForTopic(element));
+				});
+			}
+	};});
