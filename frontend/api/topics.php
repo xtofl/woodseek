9506490cdd8aa56f63724bf9fe1fe6f83f19b5ed
unknown <xtofl_2@.(none)>
2013-12-11 20:22:53 +0100

    added stress-management examples

--- frontend/api/topics.php
+++ frontend/api/topics.php
@@ -3,7 +3,7 @@
 require_once "../phpincludes/database.inc";
 
 $topics = array(
-		array("text"=>"stress-management", "id"=>"stress-management"),
+		array("text"=>"stress-management", "id"=>"stressmanagement"),
 		array("text"=>"leerstrategie", "id"=>"lerenleren"),
 		array("text"=>"zelfbeeld", "id"=>"zelfbeeld"),
 		array("text"=>"socio-emotionele ontwikkeling", "id"=>"socio-emo"),
--- frontend/phpincludes/mock_database.php
+++ frontend/phpincludes/mock_database.php
@@ -9,13 +9,23 @@ class MockDB implements iDatabase {
 	function articlesForTopics($topics)
 	{
 		$lerenleren = array(
-			new Article("lerelere1", "leren-leren", "student", "werkblaadje", "http://here.and.there", "een stapgsewijze dit en dat voor de student"),
-			new Article("lerelere2", "leren-leren", "gonbegeleider", "url", "http://hereto.and.therefrom", "praktische tips om de student dit en dat en zus en zo"));
-		$stressmanagement = array("stressmgmt1", "stressmgmt2");
-		
-		if($topics == array("lerenleren")) {
-			return $lerenleren;		
-		}
+			new Article("lerelere1", "lerenleren", "student", "werkblaadje", "http://here.and.there", "een stapgsewijze dit en dat voor de student"),
+			new Article("lerelere2", "lerenleren", "gonbegeleider", "url", "http://hereto.and.therefrom", "praktische tips om de student dit en dat en zus en zo"));
+		$stressmanagement = array
+		( new Article("stress is power", "stressmanagement", "student", "werkblaadje", "http://here.and.there", "een stapgsewijze dit en dat voor de student")
+		, new Article("burndown bij scholieren", "stressmanagement", "gonbegeleider", "url", "http://hereto.and.therefrom", "praktische tips om de student dit en dat en zus en zo")
+		, new Article("whoe pie vakantie", "stressmanagement", "student", "url", "http://hereto.and.therefromtoo", "wat als school moet en vakantie mag?")
+		);
+
+		$results = array();
+
+		if(in_array("lerenleren", $topics )) {
+			$results=array_merge($results, $lerenleren);
+		};
+		if(in_array("stressmanagement", $topics )) {
+			$results=array_merge($results, $stressmanagement);
+		};
+		return $results;
 	}
 	public function articles($constraint) {
 		return null;
