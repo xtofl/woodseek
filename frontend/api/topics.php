3b6aae4a2944edb8f4ff067c0a44a7e1b486b144
unknown <xtofl_2@.(none)>
2013-12-16 21:28:37 +0100

    added mock icons to topics

--- frontend/api/topics.php
+++ frontend/api/topics.php
@@ -3,13 +3,33 @@
 require_once "../phpincludes/database.inc";
 
 $topics = array(
-		array("text"=>"stress-management", "id"=>"stressmanagement"),
-		array("text"=>"leerstrategie", "id"=>"lerenleren"),
-		array("text"=>"zelfbeeld", "id"=>"zelfbeeld"),
-		array("text"=>"socio-emotionele ontwikkeling", "id"=>"socio-emo"),
-		array("text"=>"organisatie", "id"=>"organisatie"),
-		array("text"=>"adminstratie", "id"=>"administratie"),
-		array("text"=>"begeleidingsstrategie", "id"=>"begeleiding")
+		array("text"=>"stress-management", 
+			"id"=>"stressmanagement", 
+			"icon"=>"http://openiconlibrary.sourceforge.net/gallery2/open_icon_library-full/icons/png/48x48/emotes/face-worried.png"),
+			
+		array("text"=>"leerstrategie", 
+			"id"=>"lerenleren",
+			"icon"=>"http://openiconlibrary.sourceforge.net/gallery2/open_icon_library-full/icons/png/48x48/actions/address-book-new-2.png"),
+			
+		array("text"=>"zelfbeeld", 
+			"id"=>"zelfbeeld",
+			"icon"=>"http://openiconlibrary.sourceforge.net/gallery2/open_icon_library-full/icons/png/48x48/devices/camera-photo-5.png"),
+			
+		array("text"=>"socio-emotionele ontwikkeling", 
+			"id"=>"socio-emo",
+			"icon"=>"http://openiconlibrary.sourceforge.net/gallery2/open_icon_library-full/icons/png/48x48/devices/camera-photo-5.png"),
+			
+		array("text"=>"organisatie", 
+			"id"=>"organisatie",
+			"icon"=>"http://openiconlibrary.sourceforge.net/gallery2/open_icon_library-full/icons/png/48x48/devices/camera-photo-5.png"),
+			
+		array("text"=>"adminstratie", 
+			"id"=>"administratie",
+			"icon"=>"http://openiconlibrary.sourceforge.net/gallery2/open_icon_library-full/icons/png/48x48/devices/camera-photo-5.png"),
+			
+		array("text"=>"begeleidingsstrategie", 
+			"id"=>"begeleiding",
+			"icon"=>"http://openiconlibrary.sourceforge.net/gallery2/open_icon_library-full/icons/png/48x48/devices/camera-photo-5.png")
 );
 
 echo json_encode($topics);
