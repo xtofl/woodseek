16c37b9ba03871027572392e94b098011ac24d2d
xtofl <xtofl@server.fake>
2014-01-03 20:51:21 +0100

    added async topics call to api.js

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,10 +1,23 @@
 // implementation of api.idl in javascript.
 //
 define([], function(){
-	var state = {};
+	var state = {
+			topics: [],
+			audiences: [],
+			shapes: []
+	};
 	return {
+		connect: function(proxy) {
+			proxy.asyncTopics(function(topics){ state.topics = topics; });
+		},
 		topics: function() {
 			return state.topics;
+		},
+		audiences: function() {
+			return state.audiences;
+		},
+		shapes: function() {
+			return state.shapes;
 		}
 	};
 });
--- frontend/js/test/test_api.js
+++ frontend/js/test/test_api.js
@@ -4,7 +4,23 @@ require(
 	function(api
 	){
 		
-	test("hello", function(){
-		ok(false, "Passed");
+	test("Initial Values", function(){
+		deepEqual(api.topics(), [], "no topics should be present.");
+		deepEqual(api.audiences(), [], "no audiences should be known.");
+		deepEqual(api.shapes(), [], "no shapes should be known.");
+	});
+	
+	test("After Connection", function() {
+		var mock = {
+				asyncTopics: function(f) {
+					mock.asyncTopics.call = { f: f };
+				}
+		};
+		api.connect(mock);
+		
+		ok(mock.asyncTopics.call, "asyncTopics was called");
+		
+		mock.asyncTopics.call.f([1,2,3]);
+		deepEqual(api.topics(), [1,2,3], "When backend returns topics, they should be remembered");
 	});
 });
